2b2c9e9f7583810dda012a67c078017959470666
jose <carlosmendesbgi@gmail.com>
2022-08-09 14:39:20 -0300

    Add functions

--- B.js
+++ B.js
@@ -1,3 +1,11 @@
 console.log("Testando o git");
 
-let nome = prompt("Informe seu nome");
+//let nome = prompt("Informe seu nome"
+
+function calculadora(numero1,numero2,callback) {
+    return `O Resultdo da operação é: ${callback(numero1,numero2)}`
+};
+
+somar = (numero1,numero2) => numero1 + numero2
+
+console.log(calculadora(8,8,somar))
